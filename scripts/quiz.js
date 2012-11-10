013504cb8e923f170365fbbc3930c24df4514226
sandro <sbadame@gmail.com>
2012-11-10 18:40:36 -0500

    working boostrap

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -7,8 +7,18 @@ function QuestionControl($scope) {
     ];
 
     $scope.$on(ADD_QUESTION, function (event, newQuestion) {
+        console.log(ADD_QUESTION, newQuestion);
         $scope.questions.push(newQuestion);
+        console.log($scope.questions);
     });
+
+    $scope.shuffleChoices = function() {
+        angular.forEach($scope.questions, function(question) {
+            question.choices.push("dcasdfasdf");
+        })
+        $scope.questions.push({text: "sdfsf", choices: [1,2, 3]});
+        console.log($scope.questions)
+    };
 }
 
 function QuestionEditorControl($scope) {
@@ -31,4 +41,18 @@ function QuestionEditorControl($scope) {
                      {text: $scope.questionText, choices: $scope.choices});
         reset();
     };
+}
+
+// Procured from: http://stackoverflow.com/questions/2450954/how-to-randomize-a-javascript-array
+function shuffle(array) {
+  var i = array.length, j, tempi, tempj;
+  if ( i == 0 ) return false;
+  while ( --i ) {
+     var j = Math.floor( Math.random() * ( i + 1 ) );
+     var tempi = array[i];
+     var tempj = array[j];
+     array[i] = tempj;
+     array[j] = tempi;
+   }
+  return array;
 }
