3ce41d3f11eb01a160c54fe61b9a8613ad0c1057
sandro <sbadame@gmail.com>
2012-11-10 01:59:52 -0500

    Hey it kinda looks like a form, that was easy

---
+++ scripts/quiz.js
@@ -0,0 +1,6 @@
+function QuestionControl($scope) {
+    $scope.questions = [
+        {text: "Is the world round?", choices: ["Yes", "No", "Maybe" ]},
+        {text: "Is the world square?", choices: ["Yes", "No", "Maybe" ]}
+    ];
+}
