4708f11cea0f7f67677b8f49ea64f48f2e47ae0d
sandro <sbadame@gmail.com>
2012-11-10 11:01:49 -0500

    Yay we can add questions to the quiz

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,6 +1,34 @@
+var ADD_QUESTION = 'ADD_QUESTION';
+
 function QuestionControl($scope) {
     $scope.questions = [
-        {text: "Is the world round?", choices: ["Yes", "No", "Maybe" ]},
-        {text: "Is the world square?", choices: ["Yes", "No", "Maybe" ]}
+        {text: 'Is the world round?', choices: ['Yes', 'No', 'Maybe' ]},
+        {text: 'Is the world square?', choices: ['Yes', 'No', 'Maybe' ]}
     ];
+
+    $scope.$on(ADD_QUESTION, function (event, newQuestion) {
+        $scope.questions.push(newQuestion);
+    });
+}
+
+function QuestionEditorControl($scope) {
+
+    function reset() {
+        $scope.questionText = '';
+        $scope.choices = [];
+        $scope.choiceText = '';
+    }
+    reset();
+
+
+    $scope.addChoice = function () {
+        $scope.choices.push($scope.choiceText);
+        $scope.choiceText = '';
+    };
+
+    $scope.addQuestion = function () {
+        $scope.$emit(ADD_QUESTION,
+                     {text: $scope.questionText, choices: $scope.choices});
+        reset();
+    };
 }
