eefc0731a899670ee178ec840c43029412570e1e
sandro <sbadame@gmail.com>
2013-04-27 10:37:31 -0400

    removing the contenteditable directive

--- js/directives.js
+++ js/directives.js
@@ -10,30 +10,6 @@ quiz.directive('focusOn', function() {
     };
 });
 
-quiz.directive('contenteditable', function() {
-    return {
-        restrict: 'A', // only activate on element attribute
-        require: '?ngModel', // get a hold of NgModelController
-        link: function(scope, element, attrs, ngModel) {
-            if(!ngModel) {
-                return; // do nothing if no ng-model
-            }
-            // Specify how UI should be updated
-            ngModel.$render = function() {
-                element.html(ngModel.$viewValue || '');
-            };
-            // Listen for change events to enable binding
-            element.bind('blur keyup change', function() {
-                scope.$apply(read);
-            });
-            read(); // initialize
-            // Write data to the model
-            function read() {
-                ngModel.$setViewValue(element.html());
-            }
-        }
-    };});
-
 quiz.directive('ngEnter', function() {
     return function(scope, elm, attrs) {
         elm.bind('keypress', function(e) {
