1c8faab3799bd074bd8c795ff4dc32722d82839e
sandro <sbadame@gmail.com>
2013-08-11 00:34:48 -0400

    Now we got some sensible fixed heights

--- js/directives.js
+++ js/directives.js
@@ -6,32 +6,12 @@ quiz.directive('focusOnStart', function() {
     };
 });
 
-quiz.directive('focusOn', function() {
-    return {
-        restrict: 'A',
-        link: function(scope, iElement, iAttrs) {
-            var event_name = scope.$eval(iAttrs.focusOn);
-            scope.$on(event_name, function() {
-                iElement[0].focus();
-            });
-        }
-    };
-});
-
 quiz.directive('ngEnter', function() {
     return function(scope, elm, attrs) {
         elm.bind('keypress', function(e) {
             if (e.charCode === 13) {
                 scope.$apply(attrs.ngEnter);
             }
-        });
-    };
-});
-
-quiz.directive('ngLostFocus', function() {
-    return function(scope, elm, attrs) {
-        elm.bind('blur', function(e) {
-            scope.$apply(attrs.ngLostFocus);
         });
     };
 });
@@ -55,69 +35,13 @@ quiz.directive('contenteditable', function() {
   };
 });
 
-quiz.directive('sortable', function($timeout) {
-    return {
-        restrict: 'A',
-        require: ['ngModel'],
-        link: function(scope, element, attrs) {
-
-            var model = scope.$eval(attrs.ngModel);
-            var placeholder = scope.$eval(attrs.placeholder);
-            var indexMapper = attrs.indexMapper ? scope.$eval(attrs.indexMapper) : function(i) { return i; };
-
-            // Used to map between indicies
-            var getModelIndex = function(ui) {
-                var index = ui.placeholder.index();
-                if (index > ui.item.data('start')) {
-                    index -= 1;
-                }
-                return indexMapper(index);
-            };
-
-            // Holds the model object that the dragged element represents
-            var draggedModelObject = null;
-
-            element.sortable({
-                // Options
-                appendTo: document.body,
-                forceHelperSize: true,
-                forcePlaceholderSize: false,
-                helper: 'clone',
-                placeholder: 'placeholder',
-
-                // Events
-                start: function(event, ui) {
-                    ui.item.data('start', ui.item.index());
-                    scope.$apply(function() {
-                        draggedModelObject = model.splice(getModelIndex(ui), 1, placeholder)[0];
-                    });
-                },
-                change: function(event, ui) {
-                    var modelIndex = getModelIndex(ui);
-                    scope.$apply(function() {
-                        model.remove(placeholder);
-                        model.splice(modelIndex, 0, placeholder);
-                    });
-                },
-                stop: function(event, ui) {
-                    scope.$apply(function() {
-                        var modelIndex = model.indexOf(placeholder);
-                        model.splice(modelIndex, 1, draggedModelObject);
-                    });
-                }
-            }).disableSelection();
-
-        }
-    };
-});
-
 quiz.directive('fixedHeights', function($timeout) {
     // Make sure that the elements are the right size
     return {
         restrict: 'A',
-        require: ['ngModel'],
         link: function(scope, element, attrs) {
-            scope.$watch(attrs.ngModel, function() {
+            console.log(attrs.fixedHeights);
+            scope.$watch(attrs.fixedHeights, function() {
                 $timeout(function() {
                     element.children().each(function(childIndex) {
                         if (childIndex % 2 === 0) {
