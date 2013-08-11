3be5595d58c7d96a25ef3c3e2a667d49ab886c1f
sandro <sbadame@gmail.com>
2013-08-11 02:38:43 -0400

    Added more menu options, need to fix focus for adding a new choice.

--- js/quiz.js
+++ js/quiz.js
@@ -4,11 +4,21 @@ var quiz = angular.module('quiz', []);
  * Some javascript array augmentation that we use in our app.
  */
 
+// Procured from: http://stackoverflow.com/questions/872310/javascript-swap-array-elements
+// In place swap  of the elements in the array at indexA and indexB.
+Array.prototype.swap = function(indexA, indexB) {
+    if (indexA === indexB) {
+        return this;
+    }
+    this[indexB] = this.splice(indexA, 1, this[indexB])[0];
+    return this;
+}
+
 // Procured from: http://stackoverflow.com/questions/2450954/how-to-randomize-a-javascript-array
-Array.prototype.shuffle = function () {
+Array.prototype.shuffle = function() {
   var i = this.length, j, tempi, tempj;
   if ( i === 0 ) {
-      return false;
+      return this;
   }
   while ( --i ) {
      j = Math.floor( Math.random() * ( i + 1 ) );
